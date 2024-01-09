f7fe8faa947f2668ee818a6cf2bee1e0d449a37c
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-09 16:04:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$array = [1, 2, 3, 4, 5,];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPHotel</title>
+</head>
+
+<body>
+    <h1>Ciao</h1>
+    <?php var_dump($array) ?>
+
+    <?php echo $array ?>
+    echo '
+    <pre>';
+var_dump($variabile_array');
+echo '</pre>';
+</body>
+
+</html>
